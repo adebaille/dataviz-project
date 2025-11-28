5714bd19a2805ebceb321768e36686be42908698
Alison Debaille <adebaille@gmail.com>
2025-11-28 15:55:38 +0100

    add React Routeur, config router on main, update main.tsx

--- Dataviz/src/main.tsx
+++ Dataviz/src/main.tsx
@@ -1,11 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
+import Analyse from './Pages/Analyse'
+import About from './Pages/About'
 
+
+const router = createBrowserRouter ([
+  {
+    path : "/",
+    element: <Home />,
+  },
+ {
+    path : "/Analyse",
+    element: <Analyse />,
+  },
+   {
+    path : "/About",
+    element: <About />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
