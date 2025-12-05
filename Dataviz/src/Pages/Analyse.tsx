c491c6c9aa3f55d9cc2fd40dea6579cbed8ea53b
Alison Debaille <adebaille@gmail.com>
2025-12-05 15:27:58 +0100

    update Analyse.tsx

--- Dataviz/src/Pages/Analyse.tsx
+++ Dataviz/src/Pages/Analyse.tsx
@@ -2,15 +2,15 @@
 import Areachart from "../Components/AreaChart";
 import Linecharts from "../Components/Linecharts";
 import HorizontalBarChart from "../Components/HorizontalBarChart";
+import SimpleBarchart from "../Components/VerticalBarCharts";
 
 export default function Analyse () {
     return (
-        <h1>
+        <>
             <Linecharts/>
-            <br />
+            <SimpleBarchart />
             <Areachart />
-
             <HorizontalBarChart />
-        </h1>
+        </>
     )
 }
