d1d24b84842d4915417e70e137a1fcada8696e03
Alison Debaille <adebaille@gmail.com>
2025-12-05 12:33:07 +0100

    update HorizontalBarChart.tsx

--- Dataviz/src/Components/HorizontalBarChart.tsx
+++ Dataviz/src/Components/HorizontalBarChart.tsx
@@ -1,15 +1,23 @@
-import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  ComposedChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 import { useQuery } from "@tanstack/react-query";
 
 export default function HorizontalBarChart() {
-    // Appel API avec React Query
+  // Appel API avec React Query
   const { data, isPending, error } = useQuery({
     queryKey: ["HorizontalBarChart"],
     queryFn: async () => {
       const url = new URL(
         "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records"
       );
-    // Gestion des Query Params
+      // Gestion des Query Params
       url.searchParams.set("select", "ardt_lieu, count(*) as total");
       url.searchParams.set("group_by", "ardt_lieu");
       url.searchParams.set("where", "startswith(ardt_lieu,'75')");
@@ -33,12 +41,17 @@ export default function HorizontalBarChart() {
       });
     }
   }
-
-// Construction Graphique
+  
+  // Construction Graphique
   return (
-     <ComposedChart
+    <ComposedChart
       layout="vertical"
-      style={{ width: '100%', maxWidth: '300px', maxHeight: '70vh', aspectRatio: 1 / 1.618 }}
+      style={{
+        width: "100%",
+        maxWidth: "300px",
+        maxHeight: "70vh",
+        aspectRatio: 1 / 1.618,
+      }}
       responsive
       data={temp}
       margin={{
@@ -46,14 +59,13 @@ export default function HorizontalBarChart() {
         right: 0,
         bottom: 0,
         left: 0,
-      }}
-    >
+      }}>
       <CartesianGrid stroke="#f5f5f5" />
-      <XAxis width="auto" type="number" />
+      <XAxis type="number" />
       <YAxis dataKey="ardt" type="category" scale="band" width="auto" />
       <Tooltip />
       <Legend />
-      <Bar dataKey="total" barSize={20} fill="#7875c9ff" />
+      <Bar dataKey="total" barSize={20} fill="#413ea0" />
     </ComposedChart>
   );
 }
