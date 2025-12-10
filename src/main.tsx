e9cba2ab6558f8e87ee1f7894cd6a51ddc868856
Alison Debaille <adebaille@gmail.com>
2025-12-10 15:48:22 +0100

    Add query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -37,6 +38,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
